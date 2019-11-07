0f1da582be85346d71fcbac52553ef72ed72ded2
Khoi Hoang <hgdangkhoi@gmail.com>
2019-11-07 10:08:35 -0800

    remove bin

--- bin/SQL Scripts/DropScript.sql
+++
@@ -1,8 +0,0 @@
-drop table emissions;
-drop table emissionsCoeff;
-drop table waterCoeff;
-drop table gasInfo;
-drop table generationRange;
-drop table generation;
-drop table genTest;
-drop table facility;
